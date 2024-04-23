13d698591e0d297595b21ca429d76ceca38babc6
Hendrick Manullang <h.manullang@gmail.com>
2024-04-23 16:27:08 +1000

    script to generate random password from cli

---
+++ bin/index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+const passLength = process.argv[2] == null ? 12 : +process.argv[2];
+
+let pass = "";
+for (let i = 0; i < passLength; i++) {
+  const randCharKey = Math.floor(Math.random() * (126 - 33) + 33);
+  pass += String.fromCharCode(randCharKey);
+}
+console.log(`${pass} is copied to your clipboard`);
+pbcopy(pass);
+
+function pbcopy(data) {
+  var proc = require("child_process").spawn("pbcopy");
+  proc.stdin.write(data);
+  proc.stdin.end();
+}
